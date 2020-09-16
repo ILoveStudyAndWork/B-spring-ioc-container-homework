1be47a18a53bc580cd9204edf969102bb0de2204
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-17 01:06:29 +0800

    [fengyi_ling] feat:use Lazy

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class GreetingController {
     private final GreetingService greetingService;
 
     @Autowired
-    public GreetingController(GreetingService greetingService) {
+    public GreetingController(@Lazy GreetingService greetingService) {
         this.greetingService = greetingService;
         System.out.println("----------------init GreetingController-----------------------");
 
